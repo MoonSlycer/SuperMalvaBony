613f76ecd9ec4a8828df5ea08d1dcbd03895b57b
Charlie Wensink <cpwensink@gmail.com>
2023-04-15 14:46:03 -0400

    Create blank Unreal Engine project

---
+++ SuperMalvaBony/Source/SuperMalvaBony/SuperMalvaBonyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperMalvaBonyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERMALVABONY_API ASuperMalvaBonyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
